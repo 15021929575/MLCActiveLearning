c1c98bbf5692dcccdd33f1a99a23b416c067bd04
Dan Ringwalt <dringw@gmail.com>
2014-12-03 11:19:39 -0500

    Slow pairwise alignment

--- data/alignment.py
+++ data/alignment.py
@@ -3,22 +3,25 @@ import numpy as np
 from scipy.spatial import distance
 from Bio import pairwise2
 from Bio.SubsMat import MatrixInfo as matlist
+import multiprocessing
+def get_score(pair):
+    return pairwise2.align.localds(
+              pair[0], pair[1], matlist.blosum62, -10, -0.5)[0][2]
 
-idents = sorted(os.listdir('proteins'))
-seqs = dict()
-for ident in idents:
-    f = open('proteins/' + ident)
-    f.readline()
-    seqs[ident] = ''.join(s.strip() for s in f.readlines())
-print 'Calculating distance matrix...'
-distmat = np.empty(len(idents) * (len(idents)-1) / 2)
-ind = 0
-for i in xrange(len(idents) - 1):
-    for j in xrange(i+1, len(idents)):
-        aln_i, aln_j, score, begin, end = pairwise2.align.localds(
-            seqs[idents[i]], seqs[idents[j]], matlist.blosum62,
-            -10, -0.5)[0]
-        distmat[ind] = score
-        ind += 1
-import cPickle
-cPickle.dump(distmat, open('distmats/distmat-localalign-blosum62-10-0.5.pkl', 'wb'))
+if __name__ == "__main__":
+    pool = multiprocessing.Pool(6)
+
+    idents = sorted(os.listdir('proteins'))
+    seqs = dict()
+    for ident in idents:
+        f = open('proteins/' + ident)
+        f.readline()
+        seqs[ident] = ''.join(s.strip() for s in f.readlines())
+    print 'Calculating distance matrix...'
+    pairs = [(seqs[idents[i]],seqs[idents[j]])
+             for i in xrange(len(idents) - 1)
+             for j in xrange(i+1, len(idents))]
+    scores = map(get_score, pairs)
+    scores = np.array(scores)
+    import cPickle
+    cPickle.dump(scores, open('distmats/distmat-localalign-blosum62-10-0.5.pkl', 'wb'))
